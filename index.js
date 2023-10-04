79889be4e9265a5f52b8ef1ab781437258cd408d
zachary <zmh012004@gmail.com>
2023-10-03 22:07:16 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#000000";
-dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
-dodger.style.bottom = "0px";
-dodger.style.left = "0px";
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+undefined
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
